6bff7f85737786d13e2d8e7711fa787684649920
arpitsha26 <arpitsharma1263@gmail.com>
2025-09-16 12:17:25 +0530

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+const app=express()
+
+const url = process.env.URL;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
+
+
+
+dotenv.config()
+
+const port=process.env.PORT || 5000
+
+
+app.listen(port, () => {
+    console.log(`🚀 Server started on port: ${port}`);
+  });
